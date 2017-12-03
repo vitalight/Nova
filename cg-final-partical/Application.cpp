7d9d73a5e6bcc744201f97e8bf94fc06812ea5b7
vitalight <904844050@qq.com>
2017-12-03 20:43:08 +0800

    多钟光照混合
    十木箱一灯

--- cg-final-partical/Application.cpp
+++ cg-final-partical/Application.cpp
@@ -72,6 +72,7 @@ void Application::init()
 		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
 		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
 	};
+	
 	// first, configure the cube's VAO (and VBO)
 	unsigned int VBO;
 	glGenVertexArrays(1, &cubeVAO);
@@ -124,16 +125,64 @@ void Application::run()
 
 	// be sure to activate shader when setting uniforms/drawing objects
 	lightingShader->use();
-	lightingShader->setVec3("light.position", lightPos);
 	lightingShader->setVec3("viewPos", camera.Position);
+	lightingShader->setFloat("material.shininess", 32.0f);
 
-	// light properties
-	lightingShader->setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-	lightingShader->setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-	lightingShader->setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+	/*
+	Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index
+	the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
+	by defining light types as classes and set their values in there, or by using a more efficient uniform approach
+	by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
+	*/
+	// directional light
+	lightingShader->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+	lightingShader->setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+	lightingShader->setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+	lightingShader->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+	// point light 1
+	lightingShader->setVec3("pointLights[0].position", pointLightPositions[0]);
+	lightingShader->setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+	lightingShader->setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+	lightingShader->setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+	lightingShader->setFloat("pointLights[0].constant", 1.0f);
+	lightingShader->setFloat("pointLights[0].linear", 0.09);
+	lightingShader->setFloat("pointLights[0].quadratic", 0.032);
+	// point light 2
+	lightingShader->setVec3("pointLights[1].position", pointLightPositions[1]);
+	lightingShader->setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+	lightingShader->setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+	lightingShader->setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+	lightingShader->setFloat("pointLights[1].constant", 1.0f);
+	lightingShader->setFloat("pointLights[1].linear", 0.09);
+	lightingShader->setFloat("pointLights[1].quadratic", 0.032);
+	// point light 3
+	lightingShader->setVec3("pointLights[2].position", pointLightPositions[2]);
+	lightingShader->setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+	lightingShader->setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+	lightingShader->setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+	lightingShader->setFloat("pointLights[2].constant", 1.0f);
+	lightingShader->setFloat("pointLights[2].linear", 0.09);
+	lightingShader->setFloat("pointLights[2].quadratic", 0.032);
+	// point light 4
+	lightingShader->setVec3("pointLights[3].position", pointLightPositions[3]);
+	lightingShader->setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+	lightingShader->setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+	lightingShader->setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+	lightingShader->setFloat("pointLights[3].constant", 1.0f);
+	lightingShader->setFloat("pointLights[3].linear", 0.09);
+	lightingShader->setFloat("pointLights[3].quadratic", 0.032);
+	// spotLight
+	lightingShader->setVec3("spotLight.position", camera.Position);
+	lightingShader->setVec3("spotLight.direction", camera.Front);
+	lightingShader->setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+	lightingShader->setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+	lightingShader->setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+	lightingShader->setFloat("spotLight.constant", 1.0f);
+	lightingShader->setFloat("spotLight.linear", 0.09);
+	lightingShader->setFloat("spotLight.quadratic", 0.032);
+	lightingShader->setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+	lightingShader->setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
 
-	// material properties
-	lightingShader->setFloat("material.shininess", 64.0f);
 
 	// view/projection transformations
 	glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)A_SCR_HEIGHT / (float)A_SCR_WIDTH, 0.1f, 100.0f);
@@ -152,21 +201,35 @@ void Application::run()
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, specularMap);
 
-	// render the cube
+	// render containers
 	glBindVertexArray(cubeVAO);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		// calculate the model matrix for each object and pass it to shader before drawing
+		glm::mat4 model;
+		model = glm::translate(model, cubePositions[i]);
+		float angle = 20.0f * i;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		lightingShader->setMat4("model", model);
 
-	// also draw the lamp object
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	// also draw the lamp object(s)
 	lampShader->use();
 	lampShader->setMat4("projection", projection);
 	lampShader->setMat4("view", view);
-	model = glm::mat4();
-	model = glm::translate(model, lightPos);
-	model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-	lampShader->setMat4("model", model);
 
+	// we now draw as many light bulbs as we have point lights.
 	glBindVertexArray(lightVAO);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	for (unsigned int i = 0; i < 4; i++)
+	{
+		model = glm::mat4();
+		model = glm::translate(model, pointLightPositions[i]);
+		model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+		lampShader->setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 
 	glutSwapBuffers();
 }
--- cg-final-partical/Application.h
+++ cg-final-partical/Application.h
@@ -24,6 +24,27 @@ public:
 	void init();
 	void run();
 private:
+	// positions all containers
+	glm::vec3 cubePositions[10] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	// positions of the point lights
+	glm::vec3 pointLightPositions[4] = {
+		glm::vec3(0.7f,  0.2f,  2.0f),
+		glm::vec3(2.3f, -3.3f, -4.0f),
+		glm::vec3(-4.0f,  2.0f, -12.0f),
+		glm::vec3(0.0f,  0.0f, -3.0f)
+	};
+
 	Shader *lightingShader,
 		*lampShader;
 
